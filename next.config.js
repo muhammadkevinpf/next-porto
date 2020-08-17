6e0c4cacf8989255bd73214b57f67eb9eba0d686
muhammadkevinpf <muhammadkevinpf@gmail.com>
2020-08-17 19:33:52 +0700

    fix service worker

--- next.config.js
+++ next.config.js
@@ -3,35 +3,56 @@ const withSass = require("@zeit/next-sass");
 const withPlugins = require("next-compose-plugins");
 const runtimeCaching = require("next-pwa/cache");
 
+// module.exports = withPlugins([
+//   withSass({
+//     webpack(config, options) {
+//       config.module.rules.push({
+//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|pdf)$/,
+//         use: {
+//           loader: "url-loader",
+//           options: {
+//             limit: 500000,
+//           },
+//         },
+//       });
+
+//       return config;
+//     },
+//   }),
+//   withPWA({
+//     pwa: {
+//       dest: "public",
+//       runtimeCaching,
+//     },
+//   }),
+// ]);
+
 module.exports = withPlugins([
-  withPWA({
-    pwa: {
-      dest: "public",
-      runtimeCaching,
-    },
-  }),
-  withSass({
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|pdf)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 500000,
+  [
+    withSass,
+    {
+      webpack(config, options) {
+        config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|pdf)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 500000,
+            },
           },
-        },
-      });
+        });
 
-      return config;
+        return config;
+      },
     },
-  }),
+  ],
+  [
+    withPWA,
+    {
+      pwa: {
+        dest: "public",
+        runtimeCaching,
+      },
+    },
+  ],
 ]);
-
-// ,
-//
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     runtimeCaching,
-//   },
-// });
